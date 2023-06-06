a6f9bf2bca95c17b960408929842a090a61c7fa9
Tien Nguyen <nguyen.tien@student.greenriver.edu>
2023-06-06 14:37:41 -0700

    Documented our program

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
+/**
+ *
+ * An example program
+ *
+ * @author Tien Nguyen
+ * @version 1.0
+ */
 public class Main {
+    /**
+     * Entry point for the application
+     *
+     * @param args command line args
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
